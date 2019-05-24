f47b9d20696d10d7d5c280e6673ad8126e44fbcf
changyuyun <969842624@qq.com>
2019-05-24 14:34:11 +0800

    修改tcp服务

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -1,5 +1,5 @@
 <?php
-$http = new swoole_http_server("0.0.0.0", 9501);
+$http = new swoole_http_server("www.biihu.com.cn", 9501);
 
 $http->on('request', function($request, $response) {
     $response->end("<h1>hello swoole. #".rand(1000,9999)."</h1>");

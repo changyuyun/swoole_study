8155e7ff789898c55f8ed8e603c1671b2a69a967
changyuyun <969842624@qq.com>
2019-05-24 16:04:17 +0800

    修改tcp服务

--- demo/server/ws_server.php
+++ demo/server/ws_server.php
@@ -10,12 +10,22 @@ $server = new swoole_websocket_server("0.0.0.0", 8812);
 $server->on('open', 'onOpen');
 
 function onOpen($server, $request) {
-    print_r($request->fd);
+    var_dump($request->fd);
+    if($request->fd == 1){
+        swoole_timer_tick(2000, function($timer_id){
+            echo "2s: timerId:{$timer_id}\n";
+        });
+    }
 }
 
 //监听消息事件
 $server->on('message', function (swoole_websocket_server $server, $frame) {
     echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+
+    swoole_timer_after(5000, function() use ($server, $frame){
+        echo "5s-after\n";
+        $server->push($frame->fd, 'server-time-after:');
+    });
     $server->push($frame->fd, 'server-push-success');
 });
 

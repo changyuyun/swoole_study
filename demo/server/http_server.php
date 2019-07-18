e9c6e7b6a169e6a84459c5802b088cb7d0cce933
api_domain <969842624@qq.com>
2019-07-18 15:09:49 +0800

    Update http_server.php

--- demo/server/http_server.php
+++ demo/server/http_server.php
@@ -1,6 +1,7 @@
 <?php
 $http = new swoole_http_server("www.biihu.com.cn", 9501);
 //测试部署
+//测试部署2
 $http->set([
     'enable_static_handler' => true,
     'document_root' => "/home/work/hdtocs/swoole_study/demo/html"
